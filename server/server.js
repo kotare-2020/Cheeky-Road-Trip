b546fadc9ea7e5bc1ab8c3b4dbe817f77e3001f7
Ross Jourdain <>
2019-11-27 22:35:32 +1300

    Update deps

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 const path = require('path')
 const express = require('express')
 const cors = require('cors')
-const passport = require('passport')
 
 const authRoutes = require('./routes/auth')
 
 const server = express()
 
 server.use(cors('*'))
-server.use(passport.initialize())
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, '../public')))
