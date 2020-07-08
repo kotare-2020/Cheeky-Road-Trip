9ba2b13fa6a0edce63a9f9cba2beeba76ee2ebc9
JaydenHall <jaydenhallpt@hotmail.co.nz>
2020-07-09 10:01:13 +1200

    pulling from dev

--- seeds/found.js
+++ seeds/found.js
@@ -21,19 +21,49 @@ exports.seed = function(knex) {
         user_id: 3},
 
         {id: 4,
+        species: 'dog',
+        photo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/german-shepherd-dog-1557314959.jpg?crop=0.615xw:1.00xh;0.197xw,0&resize=480:*',
+        user_id: 4},
+  
+        {id: 5,
+        species: 'dog',
+        photo: 'https://resize.hswstatic.com/w_907/gif/dogs-embarrassed.jpg',
+        user_id: 5},
+  
+        {id: 6,
+        species: 'dog',
+        photo: 'https://scx1.b-cdn.net/csz/news/800/2019/2-dog.jpg',
+        user_id: 6},
+
+        {id: 7,
         species: 'cat',
         photo: 'https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF.jpg',
-        user_id: 4},
+        user_id: 7},
 
-        {id: 5,
+        {id: 8,
         species: 'cat',
         photo: 'https://vetstreet.brightspotcdn.com/dims4/default/2a5cd80/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F50%2Ff6%2Fc9374cbb4cdb87ab1ef6470ff619%2FMaine-Coon-AP-3OLEOB-645sm3614.jpg',
-        user_id: 5},
+        user_id: 8},
 
-        {id: 6,
+        {id: 9,
         species: 'cat',
         photo: 'https://www.aspca.org/sites/default/files/cat-care_meowing-and-yowling_main-image.jpg',
-        user_id: 6},
+        user_id: 9},
+
+        {id: 10,
+        species: 'cat',
+        photo: 'https://i.dailymail.co.uk/i/pix/2016/11/05/19/3A18DF3100000578-0-image-m-84_1478375432230.jpg',
+        user_id: 10},
+  
+        {id: 11,
+        species: 'cat',
+        photo: 'https://media14.s-nbcnews.com/j/MSNBC/Components/Video/201905/tdy_news_grumpy_cat_190517_1920x1080.focal-760x428.jpg',
+        user_id: 11},
+  
+        {id: 12,
+        species: 'cat',
+        photo: 'https://i.insider.com/549489a1ecad042f60718e6b?width=1100&format=jpeg&auto=webp',
+        user_id: 12},
       ]);
     });
 };
