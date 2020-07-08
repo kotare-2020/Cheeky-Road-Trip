aeff2da4fbb748e858a0d92955945bfe119bf285
Nick-Jarvie <nickjarvie@gmail.com>
2020-07-09 09:57:33 +1200

    update migrations ans seeds

--- seeds/lost.js
+++ seeds/lost.js
@@ -9,25 +9,25 @@ exports.seed = function(knex) {
         name: 'Harriet',
         species: 'Bull Terrier',
         photo: 'https://animals.net/wp-content/uploads/2018/07/Bull-Terrier-7-650x425.jpg',
-        user_id: null,},
+        user_id: 1,},
     
         {id: 2,
         name: 'Beans',
         species: 'Dalmatian',
         photo: 'https://thehappypuppysite.com/wp-content/uploads/2019/01/Dalmatian-Temperament-long.jpg',
-        user_id: null,},
+        user_id: 2,},
 
-        {id: 2,
+        {id: 3,
         name: 'Jarrod',
         species: 'Dachshund',
         photo: 'https://thehappypuppysite.com/wp-content/uploads/2018/12/fat-dachshund-long.jpg',
-        user_id: null,},
+        user_id: 3,},
 
-        {id: 2,
+        {id: 4,
         name: 'Cathy',
         species: 'Irish Wolfhound',
         photo: 'https://i.pinimg.com/originals/c9/9e/d7/c99ed77cb513a292e64dad5c2a966c5b.jpg',
-        user_id: null,},
+        user_id: 4,},
         
       ]);
     });
