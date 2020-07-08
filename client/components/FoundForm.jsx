49a686f29bb440f7c4d817b31e2e9bada36a0da2
George-Staniland <georgestaniland@gmail.com>
2020-07-08 19:32:38 +1200

    lil bit of work on found form

--- client/components/FoundForm.jsx
+++ client/components/FoundForm.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 
 
 class FoundForm extends React.Component {
+    
+    handleChange = (evt) => {
+        
+    }
+    
+    handleSubmit = (evt) => {
+        
+    }
+    
     render () {
         return (
-            <h3>A Found Pet</h3>
+            <>
+            <h3>Add A Found Pet</h3>
+            <form>
+                <label onSubmit= {this.handleSubmit}>
+                    Species Of Found Animal:
+                    <input type="text" name="species" onChange = {this.handleChange} />
+                </label>
+                <label>
+                    Add A Photo of Found Animal:
+                    <input type="text" name="photo" onChange = {this.handleChange} />
+                </label>
+                <input type="submit" value="Add Found Pet" />
+            </form>
+            </>
         )
     }
 }
