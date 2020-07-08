9aac2229f5d20e91549cb55237c588dd68989f78
aisyah-t <aisyah.n.tajudin@gmail.com>
2020-07-08 19:28:44 +1200

    Complete back end API

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,4 +3,22 @@ const config = require('../../knexfile')
 const env = process.env.NODE_ENV || 'development'
 const connection = knex(config[env])
 
-module.exports = connection
+function getAnimals(db = connection) {
+    return db('found').select()
+}
+
+function saveLost (lost, db = connection) {
+    return db('lost')
+        .insert({
+            user_id: lost.user_id,
+            name: lost.name,
+            species: lost.species,
+            photo: lost.photo
+        })
+
+}
+
+module.exports = {
+    getAnimals,
+    saveLost
+}
