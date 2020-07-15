9dc8eee17c58f594b4f262529cbdccaee0314966
Ben Irvine <benji72027@gmail.com>
2020-07-15 19:27:13 +1200

    add map display

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -11,6 +11,10 @@ import HomePage from './HomePage'
 import LostPets from './LostPets'
 import { checkAuth } from '../actions/auth'
 
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Icon } from 'leaflet'
+import bathroomData from '../../data/bathroom_data.json'
+
 
 export class App extends React.Component {
   componentDidMount() {
@@ -24,41 +28,51 @@ export class App extends React.Component {
   render() {
     const { auth } = this.props
     return (
-      <Router>
-        
-        <div className="container has-text-centered">
+      <>
+        <Router>
 
-          <div className="hero is-small is-primary">
-            <div className="hero-body has-text-centered">
-              <Link to='/' className="">
-                <h1 className="title is-1">Lost and Found</h1>
-              </Link>
-              <Nav />
+          <div className="container has-text-centered">
+
+            <div className="hero is-small is-primary">
+              <div className="hero-body has-text-centered">
+                <Link to='/' className="">
+                  <h1 className="title is-1">Lost and Found</h1>
+                </Link>
+                <Nav />
+              </div>
             </div>
-          </div>
 
-          <div className=''>
-            {!auth.isAuthenticated &&
-              <>
-                <Route exact path="/" component={Login} />
-                <Route path="/lost" component={LostPets} />
-              </>
-            }
-            {auth.isAuthenticated ? <HomePage /> : <></>}
-            <Route path="/home" component={HomePage} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
+            <div className=''>
+              {!auth.isAuthenticated &&
+                <>
+                  <Route exact path="/" component={Login} />
+                  <Route path="/lost" component={LostPets} />
+                </>
+              }
+              {auth.isAuthenticated ? <HomePage /> : <></>}
+              <Route path="/home" component={HomePage} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
 
+            </div>
+            <FoundPets animals={this.props.foundPets} />
           </div>
-            <FoundPets animals={this.props.foundPets}/>
-        </div>
-      </Router>
-      
+        </Router>
+        <Map center={[-39.291890, 174.267720]} zoom={12}>
+          <TileLayer
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {bathroomData.features.map((bathroom) => {
+            <Marker key={bathroom.properties.OBJECTID} position={[175.611366, -40.355660]} />
+          })}
+        </Map>
+      </>
     )
   }
 }
 
-const mapStateToProps = ({auth, foundPets}) => {
+const mapStateToProps = ({ auth, foundPets }) => {
   return {
     auth,
     foundPets
