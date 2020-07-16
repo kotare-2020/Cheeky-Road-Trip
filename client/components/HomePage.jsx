b1f4f73b9465ccd026b4d77c4fc1c1f52b6339f1
richard-eman <richardemanuel1205@gmail.com>
2020-07-17 10:38:47 +1200

    test api in HomePage.jsx

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -11,6 +11,44 @@ class HomePage extends React.Component {
     to: ''
   }
 
+  componentDidMount() {
+    console.log("env", process.env.POSITION_STACK_API_KEY)// please fix
+request.get("http://api.positionstack.com/v1/forward", {
+  'access_key': '',// key go here as string
+  //process.env.POSITION_STACK_API_KEY "how the fuck do i make work" (Richard, 2020)
+  "country": "NZ",
+  '& query': "87 Rugby Street, Palmerston North,",// address part goes here
+})
+  .then(res => {
+    // Can't push to an array WHILE setting to a variable otherwise .push() will return array length.
+    this.props.currentTrip.waypoints.inbetweenWaypoints.push({
+      buildingName: res.body.data[0].name,
+      label: res.body.data[0].label,
+      latitude: res.body.data[0].latitude,
+      longitude: res.body.data[0].longitude,
+      streetName: res.body.data[0].street,
+    })
+    const waypoints = {
+      startWaypoint: {
+        buildingName: res.body.data[0].name,
+        label: res.body.data[0].label,
+        latitude: res.body.data[0].latitude,
+        longitude: res.body.data[0].longitude,
+        streetName: res.body.data[0].street,
+      },
+      inbetweenWaypoints: this.props.currentTrip.waypoints.inbetweenWaypoints,
+      endWaypoint: {
+        buildingName: res.body.data[0].name,
+        label: res.body.data[0].label,
+        latitude: res.body.data[0].latitude,
+        longitude: res.body.data[0].longitude,
+        streetName: res.body.data[0].street,
+      },
+    }
+    this.props.dispatch(setWaypoints(waypoints))
+  })
+  }
+
   handleChange = (event) => {
     console.log('form is changing!')
   }
