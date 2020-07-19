7505944dd83e04f795fcddcd7d484900871b7ffe
George-Staniland <georgestaniland@gmail.com>
2020-07-19 16:58:49 +1200

    merge

--- client/actions/currentTrip.js
+++ client/actions/currentTrip.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-export const ADD_NEW_TRIP = "ADD_NEW_TRIP"
-export const ADD_WAYPOINTS = "ADD_WAYPOINTS"
-export const SHOW_MAP = "SHOW_MAP"
-export const CONFIRM_WAYPOINT = "CONFIRM_WAYPOINT"
-||||||| merged common ancestors
-export const ADD_NEW_TRIP = "ADD_NEW_TRIP"
-export const ADD_WAYPOINTS = "ADD_WAYPOINTS"
-export const SHOW_MAP = "SHOW_MAP"
-
-=======
 export const ADD_TRIP = "ADD_TRIP"
 export const ADD_INSTRUCTIONS = "ADD_INSTRUCTIONS"
+export const CONFIRM_WAYPOINT = "CONFIRM_WAYPOINT"
 
->>>>>>> 8353529aebff7eaddf6d0b5ec3b3b3bd9643aa3c
 
 
 export function addNewTrip(tripData) {
@@ -30,7 +19,6 @@ export function addTripInstructions(instructions) {
         type: ADD_INSTRUCTIONS,
         instructions: instructions,
     }
-<<<<<<< HEAD
 }
 
 export function confirmAddress (addressInfo) {
@@ -39,8 +27,3 @@ export function confirmAddress (addressInfo) {
         addressInfo: addressInfo
     }
 }
-||||||| merged common ancestors
-}
-=======
-}
->>>>>>> 8353529aebff7eaddf6d0b5ec3b3b3bd9643aa3c
