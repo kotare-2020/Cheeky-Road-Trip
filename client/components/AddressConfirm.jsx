801123a295d615f758fc0b0ca5ea4847b80acaed
George-Staniland <georgestaniland@gmail.com>
2020-07-19 15:19:44 +1200

    more homepage form additions

--- client/components/AddressConfirm.jsx
+++ client/components/AddressConfirm.jsx
@@ -2,12 +2,31 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 class AddressConfirm extends React.Component {
-    
+
+    addressOptions = this.props.waypointConfirmation.waypointsArray
+
     render() {
-        console.log('address confrim component props ----> ', this.props.waypointConfirmation.waypointsArray[0])
+        console.log('address confrim component props ----> ', this.addressOptions)
         return (
             <div className="address-confirm-list" >
-            <p> {this.props.waypointConfirmation.waypointsArray[0].name}</p>   
+
+                <ul>
+                    {this.addressOptions.map(addressInfo => {
+                        return (
+                            <div className="address-confirm-item">
+                                <h3><u>Is this the correct option bitch?</u></h3>
+                                <p>{addressInfo.label}</p>
+                                <p>{addressInfo.region}</p>
+                            </div>
+
+                        )
+                    })}
+                </ul>
+
+
+
+
+
             </div>
 
         )
@@ -19,8 +38,8 @@ const mapStateToProps = ({ waypointConfirmation }) => {
     return {
         waypointConfirmation
     }
-  }
-  
-  export default connect(mapStateToProps)(AddressConfirm)
-  
+}
+
+export default connect(mapStateToProps)(AddressConfirm)
+
 
